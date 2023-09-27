5468367c6f488fd835328162ec29145177586cc3
Peter <abc1870819884@gmail.com>
2023-09-28 01:27:01 +0800

    add list and range

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
-import os
 import requests
+import time
+import json
+import os
 import re
 from bs4 import BeautifulSoup
 from docx import Document
 from docx.oxml.ns import qn
 from docx.shared import Pt, RGBColor, Inches
 
-# https://mp.weixin.qq.com/s/SZ-29HwfcXmERKsHhdcoYw
-def download_images(url):
+headers = {
+    "cookie": "",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+}
+url = 'https://mp.weixin.qq.com/cgi-bin/appmsgpublish'
+
+
+def download_images(url, date):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'lxml')
     meta_element = soup.find('meta', {'property': 'og:title'})
     title = meta_element['content']
     title = re.sub('[/\:*?<>"|]', ' ', title)
-    title = "2023."+input("Please input date: ")+" "+title
+    title = time.strftime('%Y.%m.%d', date) + " " + title
     os.mkdir(title)
     os.chdir(title)
     os.mkdir(title)
@@ -23,13 +31,13 @@ def download_images(url):
     doc.styles['Normal'].font.size = Pt(10.5)
     doc.styles['Normal'].font.color.rgb = RGBColor(0, 0, 0)
     doc.add_paragraph(title)
-    tags=soup.find_all(lambda tag: tag.name == 'p' and
-                                        tag.has_attr('style') and
-                                        'text-indent: 2em;' in tag['style'] or tag.name=='img')
+    tags = soup.find_all(lambda tag: tag.name == 'p' and
+                                     tag.has_attr('style') and
+                                     'text-wrap: wrap' in tag['style'] or tag.name == 'img')
     flag = 0
     index = 0
     for tag in tags:
-        if tag.name=='img':
+        if tag.name == 'img':
             image_url = tag.get('data-src')
             if image_url is not None:
                 if flag == 1 and not image_url.endswith("svg"):
@@ -38,19 +46,58 @@ def download_images(url):
                     with open(os.path.join(title, str(index) + ".jpg"), 'wb') as f:
                         f.write(response.content)
                     doc.add_picture(os.path.join(title, str(index) + ".jpg"), width=Inches(3))
-                if image_url.endswith("svg"):
+                if "pmexJicq4T8Suxh3We8h1H2T03VibiaibDu1PCsIQWDia7ue76uSfaadVrDsCn9CWGRibaiaxGicj6eyapMibWtS6DFsxOA" in image_url:
                     flag = flag + 1
-        else:
+        elif flag == 1:
+            if "文稿撰写：" in tag.text:
+                flag = flag + 1
+                break
             content = tag.text
             doc.add_paragraph(content)
     # if not os.path.exists(title):
     doc.save(title + '.docx')
 
 
+def page(num=1):
+    title = []
+    link = []
+    sent_time = []
+    for i in range(num):
+        data = {
+            'sub': 'list',
+            'begin': 0,
+            'count': 10,
+            'query': '',
+            'type': '101_1',
+            'show_type': '',
+            'free_publish_type': 1,
+            'sub_action': 'list_ex',
+            'search_card': 1,
+            'token': 225480359,
+            'lang': 'en_US',
+            'f': 'json',
+            'ajax': 1
+        }
+        r = requests.get(url, headers=headers, params=data)
+        dic = json.loads(r.json()['publish_page'])
+        for page in dic['publish_list']:
+            page_info = json.loads(page['publish_info'])
+            if page_info['type'] == 1:
+                page_time = time.localtime(page_info['publish_info']['update_time'])
+                sent_time.append(page_time)
+                title.append(page_info['appmsgex'][0]['title'])
+                link.append(page_info['appmsgex'][0]['link'])
+    return (sent_time, title, link)
+
+
 if __name__ == '__main__':
-    print("Made By Jing. I love you~")
-    url = input("Please input url:")
-    download_images(url)
-    print("Success.")
-    os.system('explorer ..')
-    os.system('pause')
+
+    (tim, tle, lik) = page(1)
+    for id, (x, y, z) in enumerate(zip(tim, tle, lik), start=1):
+        print(id, time.strftime('%Y年%m月%d日', x), y)
+    id1 = int(input('Enter id1: '))
+    id2 = int(input('Enter id2: '))
+    for i in range(id1 - 1, id2):
+        download_images(lik[i], x)
+        # download_images("https://mp.weixin.qq.com/s/wYae_8I1o9kMnZBzdmixAA", x)
+        print(tle[i] + ' has been downloaded!')
